4c7965940e2cd2a043a7261076bcd1399ad27ab3
nitosan <nite.luo@samsung.com>
2017-09-25 10:57:29 -0700

    Add archive script

---
+++ archive-template.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+ECHO. *************************************
+ECHO. ** Archive Gearvrf template to zip **
+ECHO. *************************************
+
+git archive --format zip --output ../template.zip master 
+
